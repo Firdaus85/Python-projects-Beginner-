9d8e119fc6518c65ad55e18028ee970397bf8f76
Firdaus85 <abdulkareemfirdaus20@gmail.com>
2022-04-28 20:18:04 +0000

    Added some changes

--- simpleCalculator.py
+++ simpleCalculator.py
@@ -5,67 +5,87 @@
 
 
 # Take inputs from user and store it
-
-operation= input(
-"""
-Please what operation will you like to perform ? Type
-1. For Addition
-2. For Subtraction
-3. For multiplication
-4. For division
-
-"""
-)
+from ast import If
 
 
-num1= int(input("Please enter first number : "))
-num2= int(input("Please enter second number : "))
+def calculate():
+    operation= input(
+    """
+    Please what operation will you like to perform ? Type
+    1. For Addition
+    2. For Subtraction
+    3. For multiplication
+    4. For division
 
-# Define an addition function
-
-def add(num1, num2):
-    return num1 + num2
-
-
-# Define a subtraction funtion
-def subtract(num1, num2):
-    return num1 - num2
-
-
-# Define a multiplication function
-def multiply(num1, num2):
-    return num1 * num2
+    """
+    )
 
 
-# Define a division function
-def divide(num1, num2):
-    return num1 / num2
+    num1= int(input("Please enter first number : "))
+    num2= int(input("Please enter second number : "))
+
+    # Define an addition function
+
+    def add(num1, num2):
+        return num1 + num2
 
 
+    # Define a subtraction funtion
+    def subtract(num1, num2):
+        return num1 - num2
 
 
+    # Define a multiplication function
+    def multiply(num1, num2):
+        return num1 * num2
 
-# The blocks of code below are conditional statement which checks the operation choosen and subsequently executes the function
 
-
-if operation == "1":
-    print('{} + {}'.format(num1, num2))
-    print(add(num1, num2))
-elif operation == "2":
-    print('{} - {}'.format(num1, num2))
-    print(subtract(num1, num2))
-elif operation == "3":
-    print('{} x {}'.format(num1, num2))
-    print(multiply(num1, num2))
-elif operation == "4":
-    print('{} / {}'.format(num1, num2))
-    print(divide(num1, num2))
-else:
-    print("You have not typed a valid operation number. Restart to choose a valid operation number ")
+    # Define a division function
+    def divide(num1, num2):
+        return num1 / num2
 
 
 
 
 
+    # The blocks of code below are conditional statement which checks the operation choosen and subsequently executes the function
 
+
+    if operation == "1":
+        print('{} + {}'.format(num1, num2))
+        print(add(num1, num2))
+    elif operation == "2":
+        print('{} - {}'.format(num1, num2))
+        print(subtract(num1, num2))
+    elif operation == "3":
+        print('{} x {}'.format(num1, num2))
+        print(multiply(num1, num2))
+    elif operation == "4":
+        print('{} / {}'.format(num1, num2))
+        print(divide(num1, num2))
+    else:
+        print("You have not typed a valid operation number. Restart to choose a valid operation number ")
+
+    calculate_again()
+
+
+
+def calculate_again():
+    calc_again = input("""
+    Will you like to calculate again? 
+    1. Yes
+    2. No
+    """)
+
+    if calc_again == "1":
+        calculate()
+    elif calc_again =="2":
+        print("See you again...")
+    else:
+        calculate_again()
+
+
+
+
+calculate()
 
